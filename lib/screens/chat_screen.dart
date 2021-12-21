c15daf0bfed960f2f54bff5c95ddc3571dbd6c80
atharv-bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2021-12-21 22:28:04 +0530

    Create chat_app

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Chat App',
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, idx) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            'Message',
+          ),
+        ),
+      ),
+    );
+  }
+}
